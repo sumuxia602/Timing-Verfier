40570d775597f633e6bc68930087216618d8897a
LYLin520 <3836995751@qq.com>
2025-02-14 11:44:41 +0800

    Update run_benchmark_cli.py

--- ARM64/run_benchmark_cli.py
+++ ARM64/run_benchmark_cli.py
@@ -2,11 +2,9 @@ import os
 import warnings
 import argparse
 import json
-import sys
 
 from src.util import read_config, Logger
 from core import end2end_run
-from corev2 import end2end_runv2
 
 warnings.filterwarnings('ignore', category=UserWarning)
 
@@ -26,13 +24,13 @@ def parse_args():
     # args.skip_user_plt
     parser.add_argument('--skip_user_plt', action='store_true',
                         help="Whether to skip the user's plt. If not provided, the default is false.")
-    raw_command_line = ' '.join(sys.argv)
+
     args = parser.parse_args()
-    return args, raw_command_line
+    return args
 
 
 if __name__ == "__main__":
-    args, raw_command_line = parse_args()
+    args = parse_args()
 
     tb_config = read_config(args.tbpath, gen_procedure_cfg=args.gen_procedure_cfg, skip_user_plt=args.skip_user_plt)
     logger = Logger(verbose=args.verbose)
@@ -44,8 +42,7 @@ if __name__ == "__main__":
     # )
 
     # 使用CFG的更新方法
-    end2end_runv2(
+    end2end_run(
         config=tb_config,
-        logger=logger,
-        command=raw_command_line
+        logger=logger
     )
